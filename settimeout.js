82e60370d3bce623f8a9ef824a5a82809216ae6e
Ritesh Maurya <ayanmaurya89@gmail.com>
2025-04-27 15:20:14 +0530

    done

---
+++ settimeout.js
@@ -0,0 +1,8 @@
+console.log("Message 1");
+
+// Set a timeout to print Message 2 after 2 seconds
+setTimeout(function() {
+  console.log("Message 2 after 2 seconds");
+}, 2000);
+
+console.log("Message 3");
