d115e10e916f1a2f996ed9f5ad637cd16c5166a0
Ritesh Maurya <ayanmaurya89@gmail.com>
2025-04-27 15:03:42 +0530

    done

---
+++ index.js
@@ -0,0 +1,13 @@
+// Create the student object
+let student = {
+    name: "Alice",
+    age: 22,
+    course: "Computer Science"
+  };
+  
+  // Convert the object to a JSON string
+  let jsonString = JSON.stringify(student);
+  
+  // Log the JSON string to the console
+  console.log(jsonString);
+  
